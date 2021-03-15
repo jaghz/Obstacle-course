169757b35cdbb7132d5ee146da885cf8a397b709
jaghz <jill.a.zalat@gmail.com>
2021-03-15 11:23:08 -0400

    Time.deltaTime and moveSpeed

--- Assets/Mover.cs
+++ Assets/Mover.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class Mover : MonoBehaviour
 {
+    [SerializeField] float moveSpeed = 10f;
     // Start is called before the first frame update
     void Start()
     {
@@ -14,8 +15,9 @@ public class Mover : MonoBehaviour
     void Update()
     {
         // float xValue, yValue, zValue are stored in Update() bc they must read in the value every frame for object to move!
-        float xValue = Input.GetAxis("Horizontal"); // Are you pushing left or right (or a or d) on the keyboard?
-        float zValue = Input.GetAxis("Vertical"); // Pushing up or down (or w or s) on the keyboard?
+        //Time.deltaTime makes the movement frame rate independent
+        float xValue = Input.GetAxis("Horizontal")*Time.deltaTime*moveSpeed; // Are you pushing left or right (or a or d) on the keyboard?
+        float zValue = Input.GetAxis("Vertical")*Time.deltaTime*moveSpeed; // Pushing up or down (or w or s) on the keyboard?
 
         transform.Translate(xValue,0,zValue); //Input.something allows us to access the Input Manager (Edit>Project Settings>Input Manager)
                                                                           //Which has predefined keys for horiz/vertical movements
