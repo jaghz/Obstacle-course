c6d29aa462c01635aef4339ce6d933390b12737e
jaghz <jill.a.zalat@gmail.com>
2021-03-19 08:14:57 -0400

    Added hit counter + new obstacle

--- Assets/ObjectHit.cs
+++ Assets/ObjectHit.cs
@@ -7,7 +7,6 @@ public class ObjectHit : MonoBehaviour
     private void OnCollisionEnter(Collision other)
     {
         // Signal when cubey hits the wall
-        Debug.Log("Bumped into a wall");
         GetComponent<MeshRenderer>().material.color = Color.yellow;
     }
 }
---
+++ Assets/Scorer.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Scorer : MonoBehaviour
+{
+    int hits = -1;
+    private void OnCollisionEnter(Collision collision)
+    {
+        hits++;
+        Debug.Log("You've bumped into something this many times: " + hits);
+    }
+}
